a90b3f3779d3f057b6fb7b54e228413ec9bd6d0f
benbrahimmouniba <benbrahim.mouniba@gmail.com>
2025-10-27 21:27:49 +0100

    Ajout favicon EPC2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 // Dans app/layout.tsx
 import type { Metadata } from 'next';
 export const metadata: Metadata = {
-  title: 'EPC - ENPO Maurice Audin | Engineering Pioneers Club',
+  title: 'EPC - ENPO Maurice Audin ',
   description: 'Site officiel du Engineering Pioneers Club - École Nationale Polytechnique d\'Oran Maurice Audin',
   icons: {
     icon: '/favicon.ico',
